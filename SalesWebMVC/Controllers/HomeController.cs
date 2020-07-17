7f0cb39a366d4e1672017f0871268623aea44b09
Madara701 <matos701@gmail.com>
2020-07-17 13:43:47 -0300

    primeira controller

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -13,13 +13,14 @@ namespace SalesWebMVC.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course.";
 
             return View();
         }
 
         public IActionResult Contact() {
             ViewData["Message"] = "Your contact page.";
+            
 
             return View();
         }
